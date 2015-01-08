ca8e304518d50df9709b5dd3a40ad1f78242631b
nadiel <nadielmehdi@gmail.com>
2015-01-08 12:54:50 +0100

    ^^

--- src/m/connectionBDD.py
+++ src/m/connectionBDD.py
@@ -1,7 +1,7 @@
 import sqlite3
 class connectionBDD:
     def __init__(self):
-        self.chemin = "/Users/nadiel/SQLite/BDDrojetPython.sq3"
+        self.chemin = "/Users/nadiel/SQLite/BDDprojetPython.sq3"
         self.conn =sqlite3.connect(self.chemin)
         self.cur =self.conn.cursor()
     def seDeconnecter(self):
--- src/m/service.py
+++ src/m/service.py
@@ -1,5 +1,5 @@
 import time
-
+import calendar
 import datetime
 from src.m.connectionBDD import connectionBDD
 class service:
@@ -19,12 +19,18 @@ class service:
     def enregistrerService(self):
         try:
             connection = connectionBDD()
-            #obtenir id service (fonction max de sqlite ne marche pas bien...elle ne prend en compte que le premier chiffre. Ex: max(56,9)= 9... )
+            #1.obtenir id service (fonction max de sqlite ne marche pas bien...elle ne prend en compte que le premier chiffre. Ex: max(56,9)= 9... )
             connection.cur.execute("SELECT count(service.idSercice) FROM service;")
+            #entrée dans la base de donnée
             indiceidSer= connection.cur.execute("SELECT count(service.idService) FROM service;")
             idService = int(''.join(map(str,indiceidSer))) +1
-            #entrée dans la base de donnée
-            connection.cur.execute("INSERT INTO service (idService,dateService,dateDemande,rapport,idClient,idVoiturier,idService, idClient, idVoiturier) VALUES (?,?,?,?,?,?,?,?,?);",(idService, self.dateService, self.dateDemande, self.rapport) )
+            #2. obtenir l'idClient: définir une variable globale lors de l'execution du "jeu"
+            #3.Obtenir l'idVoiturier: en recherchant celui qui est disponible le jour en question
+            jour= calendar.weekday(self.dateService._day, self.dateService._month, self.dateService._year)
+            indiceVoiturier= connection.cur.execute("SELECT voiturier.idVoiturier FROM voiturier WHERE voiturier.joursDisponible = ?; ", (jour) )
+            idVoiturier = int(''.join(map(str,indiceVoiturier )))
+            connection.cur.execute("INSERT INTO service (idService,dateService,dateDemande,rapport,idClient,idVoiturier,idService, idVoiturier) VALUES (?,?,?,?,?,?,?,?,?);",(idService, self.dateService, self.dateDemande, self.rapport, idClient, idVoiturier, idService) )
+            connection.seDeconnecter()
         except Exception, e:
             print str(e)
             pass
